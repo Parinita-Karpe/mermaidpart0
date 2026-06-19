4dd174f8eab9784c04ce2f834d339faeb44356d5
Parinita Karpe <parinitaamolkarpe@gmail.com>
2026-06-19 18:52:41 +0530

    Complete ex 2.19

--- Part2/src/App.jsx
+++ Part2/src/App.jsx
@@ -23,13 +23,14 @@ const App = () => {
    <div>
     find countries
     <input value={search} onChange={handleSearchChange} />
-    {filteredCountries.length > 10 && (<p>Too many matches, specify another filter</p>)}
-    {filteredCountries.length<=10 && filteredCountries.length>1 && filteredCountries.map(country => <p key={country.cca3}>{country.name.common}</p>)}
+    {search !== '' && filteredCountries.length > 10 && (<p>Too many matches, specify another filter</p>)}
+    {filteredCountries.length<=10 && filteredCountries.length>1 && filteredCountries.map(country => ( <div key={country.cca3}>{country.name.common}
+    <button onClick={() => setSearch(country.name.common)}>show</button></div>))}
    
    {filteredCountries.length === 1 && (
     <div>
       <h1>{filteredCountries[0].name.common}</h1>
-      <p>capital {filteredCountries[0].capital}</p>
+      <p>capital {filteredCountries[0].capital[0]}</p>
       <p>area {filteredCountries[0].area}</p>
       <h2>languages:</h2>
       <ul>
