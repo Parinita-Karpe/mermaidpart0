02e59c1371c14dd2a4131079d5b06fcea0f7780b
Parinita Karpe <parinitaamolkarpe@gmail.com>
2026-06-21 22:26:58 +0530

    Complete ex 3.8

--- Part3/index.js
+++ Part3/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
 let persons = [
   {
     id: "1",
@@ -93,7 +93,11 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 
