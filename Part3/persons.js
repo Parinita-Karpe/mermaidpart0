633913bb44b13b404cd1a35711febc4f8f72ec6a
Parinita Karpe <parinitaamolkarpe@gmail.com>
2026-06-24 08:25:40 +0530

    Complete ex 3.13

--- Part3/persons.js
+++ Part3/persons.js
@@ -10,4 +10,27 @@ const create=newPerson=>{
     return axios.post(baseUrl,newPerson).then(response=>response.data)
 }
 
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+personSchema.set('toJSON',
+    {
+        transform: (document,returnedobject)=>{
+            returnedobject.id=returnedobject._id.toString()
+            delete returnedobject._id
+            delete returnedobject.__v
+        }
+    }
+)
+
+module.exports = mongoose.model('Person', personSchema)
 export default{getAll, create}
